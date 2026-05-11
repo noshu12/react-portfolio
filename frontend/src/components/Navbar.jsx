c05328a169ad5f0951e7f286ad46d105be9f930e
noshu12 <alamnoushad081@gmail.com>
2026-05-11 05:02:52 +0500

    Final

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -22,9 +22,9 @@ export default function Navbar({ scrollY, isDark, toggleTheme }) {
 
         <ul className={`nav-menu ${isOpen ? 'active' : ''}`}>
           <li><Link to="/" onClick={() => setIsOpen(false)}>Home</Link></li>
+          <li><Link to="/about" onClick={() => setIsOpen(false)}>About</Link></li>
           <li><Link to="/services" onClick={() => setIsOpen(false)}>Services</Link></li>
           <li><Link to="/project" onClick={() => setIsOpen(false)}>Project</Link></li>
-          <li><Link to="/about" onClick={() => setIsOpen(false)}>About</Link></li>
           <li><Link to="/contact" onClick={() => setIsOpen(false)}>Contact</Link></li>
         </ul>
 
